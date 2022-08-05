a4f09b5683a24e81021b59f9b7213651408eade9
Kevin <kebbo2110@gmail.com>
2022-08-05 09:08:27 +0200

    creating a constructor

--- src/it/develhope/ProvaRepo/entities/Animal.java
+++ src/it/develhope/ProvaRepo/entities/Animal.java
@@ -4,4 +4,11 @@ public class Animal {
     private String name;
     private String type;
     private boolean hasFour;
+
+    public Animal(String name, String type, boolean hasFour){
+        this.name = name;
+        this.type = type;
+        this.hasFour = hasFour;
+    }
+
 }
